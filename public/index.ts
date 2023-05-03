b5766c76ef1b3864745b9c75d518006ad74e9b13
Kian2002 <canadakurdistan@gmail.com>
2023-05-03 02:59:07 -0700

    add hero images

--- public/index.ts
+++ public/index.ts
@@ -1,4 +1,6 @@
 import * as menu from './bars-solid.svg';
 import * as menuOpen from './x-solid.svg';
+import * as hero from './body.png';
+import * as mobileHero from './mobile-body.png'
 
-export { menu, menuOpen };
+export { menu, menuOpen, hero, mobileHero };
