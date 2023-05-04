ff6ff3d56053fcc8e3e781e41936729faca7c53f
Kian2002 <canadakurdistan@gmail.com>
2023-05-03 22:20:10 -0700

    add images

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,8 @@
 import * as menu from './bars-solid.svg';
 import * as menuOpen from './x-solid.svg';
 import * as hero from './body.png';
-import * as mobileHero from './mobile-body.png'
+import * as mobileHero from './mobile-body.png';
+import * as flying from './flying.png';
+import * as starwars from './starwars.png';
 
-export { menu, menuOpen, hero, mobileHero };
+export { menu, menuOpen, hero, mobileHero, flying, starwars};
