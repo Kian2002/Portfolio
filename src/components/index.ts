ccf0e936f1b7ea9b2046f2b88798e34cf44fb9bd
Kian2002 <canadakurdistan@gmail.com>
2023-05-05 21:57:07 -0700

    add socials & menu components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ import Hero from "./Hero";
 import About from "./About";
 import Orb from "./Orb";
 import ScrollNotifier from "./ScrollNotifier";
+import Socials from "./Socials";
+import Menu from "./Menu";
 
-export { Navbar, Hero, About, Orb, ScrollNotifier };
+export { Navbar, Hero, About, Orb, ScrollNotifier, Socials, Menu };
