3cac284e704e7ea5972b09f7b3747865c084d9cc
Kian2002 <canadakurdistan@gmail.com>
2023-05-05 19:39:53 -0700

    add icons for niche section

--- public/index.ts
+++ public/index.ts
@@ -4,5 +4,9 @@ import * as hero from './body.png';
 import * as mobileHero from './mobile-body.png';
 import * as flying from './flying.png';
 import * as starwars from './starwars.png';
+import * as web from './icons/frontend.svg';
+import * as backend from './icons/backend.svg';
+import * as mobile from './icons/mobile.svg';
+import * as software from './icons/software.svg';
 
-export { menu, menuOpen, hero, mobileHero, flying, starwars};
+export { menu, menuOpen, hero, mobileHero, flying, starwars, web, backend, mobile, software};
