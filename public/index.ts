2c9d0e6cdf57739362a0f6cf5c8f85c84c79a7a8
Kian2002 <canadakurdistan@gmail.com>
2023-05-10 22:45:30 -0700

    add skills icons

--- public/index.ts
+++ public/index.ts
@@ -1,16 +1,69 @@
-import * as menu from './bars-solid.svg';
-import * as menuOpen from './x-solid.svg';
-import * as hero from './body.png';
-import * as mobileHero from './mobile-body.png';
-import * as flying from './flying.png';
-import * as starwars from './starwars.png';
-import * as web from './icons/frontend.svg';
-import * as backend from './icons/backend.svg';
-import * as mobile from './icons/mobile.svg';
-import * as software from './icons/software.svg';
-import * as linkedin from './socials/linkedin.svg';
-import * as github from './socials/square-github.svg';
-import * as resume from './socials/file-solid.svg'
-import * as earls from './earls.png';
+import * as menu from "./bars-solid.svg";
+import * as menuOpen from "./x-solid.svg";
+import * as hero from "./body.png";
+import * as mobileHero from "./mobile-body.png";
+import * as flying from "./flying.png";
+import * as starwars from "./starwars.png";
+import * as web from "./icons/frontend.svg";
+import * as backend from "./icons/backend.svg";
+import * as mobile from "./icons/mobile.svg";
+import * as software from "./icons/software.svg";
+import * as linkedin from "./socials/linkedin.svg";
+import * as github from "./socials/square-github.svg";
+import * as resume from "./socials/file-solid.svg";
+import * as earls from "./earls.png";
+import * as html from "./skills/html5.svg";
+import * as css from "./skills/css3-alt.svg";
+import * as js from "./skills/js.svg";
+import * as ts from "./skills/ts.svg";
+import * as react from "./skills/react.svg";
+import * as next from "./skills/nextjs.svg";
+import * as node from "./skills/node-js.svg";
+import * as java from "./skills/java.svg";
+import * as cpp from "./skills/cpp.svg";
+import * as lua from "./skills/lua.svg";
+import * as ejs from "./skills/ejs.svg";
+import * as express from "./skills/express.svg";
+import * as bootstrap from "./skills/bootstrap.svg";
+import * as tailwind from "./skills/tailwind.svg";
+import * as mongo from "./skills/mongo.svg";
+import * as postgres from "./skills/postgres.svg";
+import * as firebase from "./skills/firebase.svg";
+import * as postman from "./skills/postman.svg";
+import * as git from "./skills/git-alt.svg";
 
-export { menu, menuOpen, hero, mobileHero, flying, starwars, web, backend, mobile, software, linkedin, github, resume, earls};
+export {
+  menu,
+  menuOpen,
+  hero,
+  mobileHero,
+  flying,
+  starwars,
+  web,
+  backend,
+  mobile,
+  software,
+  linkedin,
+  github,
+  resume,
+  earls,
+  html,
+  css,
+  js,
+  ts,
+  react,
+  next,
+  node,
+  java,
+  cpp,
+  lua,
+  express,
+  ejs,
+  bootstrap,
+  tailwind,
+  mongo,
+  postgres,
+  firebase,
+  postman,
+  git,
+};
