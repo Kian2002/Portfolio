56dabbff7603cc62fd6368f60484df37d34b207a
Kian2002 <canadakurdistan@gmail.com>
2023-05-12 22:07:18 -0700

    add projectCard

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,51 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
+import { ProjectProps } from "@/constants/types";
+import { projects } from "@/constants";
+
+const ProjectCard: React.FC<ProjectProps> = ({ project }) => {
+  return (
+    <Tilt
+      className="w-full rounded-2xl shadow-xl cursor-pointer bg-secondary p-5 sm:w-[360px]"
+      tiltMaxAngleX={10}
+      tiltMaxAngleY={10}
+      glareEnable={true}
+      glareMaxOpacity={0.45}
+      glarePosition="all"
+      scale={1.05}
+      transitionSpeed={2500}
+      gyroscope={true}
+    >
+      <Link href={project.link} passHref target="_blank">
+        <div className="relative w-full h-[230px]">
+          <Image
+            src={project.image}
+            alt={project.title}
+            height={100}
+            width={100}
+            className="rounded-2xl w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="flex flex-col justify-between mt-5">
+          <h3 className="text-white font-black text-[30px]">{project.title}</h3>
+          <p className="text-tertiary text-[14px]">{project.description}</p>
+        </div>
+
+        <div>
+          {project.tags.map((tag, index) => (
+            <span key={index} className="text-tertiary text-[14px]">
+              {tag} {index !== project.tags.length - 1 && "/ "}
+            </span>
+          ))}
+        </div>
+      </Link>
+    </Tilt>
+  );
+};
 
 const Projects = () => {
   return (
@@ -20,6 +66,12 @@ const Projects = () => {
           Projects.
         </h2>
       </motion.div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-20">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
     </section>
   );
 };
