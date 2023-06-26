fd65609296fde79735b490b9559fdb0f5271e2d4
Kian2002 <canadakurdistan@gmail.com>
2023-06-25 21:19:54 -0700

    remove avatar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,15 @@ import { Orb, ScrollNotifier, Socials } from "@/components";
 
 const Hero = () => {
   return (
-    <section className="relative w-full min-h-screen mx-auto max-w-7xl overflow-hidden">
-      <div className="max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5">
+    <section className="relative w-full min-h-screen mx-auto max-w-7xl overflow-hidden flex flex-col items-center justify-center">
+      <div className="max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-center justify-center gap-5">
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-primary" />
           <div className="w-1 sm:h-72 h-40 hero-gradient" />
         </div>
 
         <div>
-          <h1 className="font-black text-white sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
+          <h1 className="font-black text-tertiary sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
             {"Hi, I'm"}{" "}
             <motion.span
               initial={{ opacity: 0, x: -100 }}
@@ -28,7 +28,7 @@ const Hero = () => {
               Kian
             </motion.span>
           </h1>
-          <p className="text-white font-medium sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100">
+          <p className="text-tertiary font-medium sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100">
             I develop Fullstack applications, mobile{" "}
             <br className="sm:block hidden" />
             apps, and dabble in some game development.
@@ -37,7 +37,6 @@ const Hero = () => {
         </div>
       </div>
 
-      <Orb />
       <ScrollNotifier />
     </section>
   );
